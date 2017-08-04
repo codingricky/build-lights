a1d85c5e36f78828f16d436606c2a691d617f5de
Ricky <ricky@dius.com.au>
2017-08-04 16:27:09 +1000

    more mods

---
+++ light-controller/ci_server/tabletennis.py
@@ -0,0 +1,39 @@
+import jenkinsapi
+from jenkinsapi.jenkins import Jenkins
+from lib.constants import STATUS
+from lib import logger
+
+_STATUS = {
+    'aborted'         : STATUS.ABORTED,
+    'aborted_anime'   : STATUS.BUILDING_FROM_ABORTED,
+    'blue'            : STATUS.SUCCESS,
+    'blue_anime'      : STATUS.BUILDING_FROM_SUCCESS,
+    'disabled'        : STATUS.DISABLED,
+    'disabled_anime'  : STATUS.BUILDING_FROM_DISABLED,
+    'grey'            : STATUS.UNKNOWN,
+    'grey_anime'      : STATUS.BUILDING_FROM_UNKNOWN,
+    'notbuilt'        : STATUS.NOT_BUILT,
+    'notbuilt_anime'  : STATUS.BUILDING_FROM_NOT_BUILT,
+    'red'             : STATUS.FAILURE,
+    'red_anime'       : STATUS.BUILDING_FROM_FAILURE,
+    'yellow'          : STATUS.UNSTABLE,
+    'yellow_anime'    : STATUS.BUILDING_FROM_UNSTABLE
+}
+
+class Source():
+
+    def __init__(self, api_token):
+        self.api_token = api_token
+        self.logger = logger.Logger('tabletennis')
+
+    def list_projects(self):
+        return list(1, 2, 3)
+
+    def project_status(self, project, branch='master'):
+        try:
+            result = 'blue'
+        except Exception, e:
+            self.logger.log("Error while computing state for project '%s': %s", project, str(e))
+            return STATUS.POLL_ERROR
+
+        return _STATUS[result['color']]
